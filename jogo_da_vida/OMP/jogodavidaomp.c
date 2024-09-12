fd0f6809ff6c7e39aac41cb1e5387b5778209188
Guilherme Nishimura <78215376+Guilherme-Nishi@users.noreply.github.com>
2024-09-12 15:10:08 -0300

    Update jogodavidaomp.c

--- jogo_da_vida/OMP/jogodavidaomp.c
+++ jogo_da_vida/OMP/jogodavidaomp.c
@@ -2,101 +2,146 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <omp.h>
-#include <curl/curl.h>
+#include <string.h>
+#include <curl/curl.h> // Inclua a biblioteca curl para HTTP requests
 
-#define ind2d(i,j) (i)*(tam+2)+j
-#define POWMIN 2
-#define POWMAX 10
+#define ind2d(i,j) ((i)*(tam+2)+(j))
 
 double wall_time(void) {
-  struct timeval tv;
-  struct timezone tz;
-  gettimeofday(&tv, &tz);
-  return(tv.tv_sec + tv.tv_usec/1000000.0);
-}
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv, &tz);
+    return(tv.tv_sec + tv.tv_usec/1000000.0);
+} /* fim-wall_time */
 
-// Função para enviar dados ao Elasticsearch
-void send_to_elasticsearch(int tam, double init_time, double comp_time, double total_time) {
-  CURL *curl;
-  CURLcode res;
-  
-  // Inicializa o cURL
-  curl = curl_easy_init();
-  if(curl) {
-    // Monta o JSON com os dados
-    char json_data[256];
-    snprintf(json_data, sizeof(json_data),
-             "{\"tam\": %d, \"init_time\": %.7f, \"comp_time\": %.7f, \"total_time\": %.7f}",
-             tam, init_time, comp_time, total_time);
-    
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9200/game-of-life/_doc/");
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data);
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    
-    // Executa a requisição
-    res = curl_easy_perform(curl);
-    
-    // Verifica o resultado
-    if(res != CURLE_OK)
-      fprintf(stderr, "Erro ao enviar dados para o Elasticsearch: %s\n", curl_easy_strerror(res));
-    
-    // Finaliza cURL
-    curl_easy_cleanup(curl);
-  }
+// Função para enviar dados para o Elasticsearch
+void SendDataToElasticsearch(const char* data) {
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if(curl) {
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9200/indice/_doc/");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    }
+    curl_global_cleanup();
 }
 
 void UmaVida(int* tabulIn, int* tabulOut, int tam) {
-  int i, j, vizviv;
-  #pragma omp parallel for private(j, vizviv) shared(tabulIn, tabulOut, tam)
-  for (i = 1; i <= tam; i++) {
-    for (j = 1; j <= tam; j++) {
-      vizviv = tabulIn[ind2d(i-1,j-1)] + tabulIn[ind2d(i-1,j )] +
-               tabulIn[ind2d(i-1,j+1)] + tabulIn[ind2d(i ,j-1)] +
-               tabulIn[ind2d(i ,j+1)] + tabulIn[ind2d(i+1,j-1)] +
-               tabulIn[ind2d(i+1,j )] + tabulIn[ind2d(i+1,j+1)];
+    int i, j, vizviv;
+    // Paralelize o loop externo com OpenMP
+    #pragma omp parallel for private(j, vizviv) shared(tabulIn, tabulOut, tam)
+    for (i = 1; i <= tam; i++) {
+        for (j = 1; j <= tam; j++) {
+            vizviv = tabulIn[ind2d(i-1,j-1)] + tabulIn[ind2d(i-1,j )] +
+                     tabulIn[ind2d(i-1,j+1)] + tabulIn[ind2d(i ,j-1)] +
+                     tabulIn[ind2d(i ,j+1)] + tabulIn[ind2d(i+1,j-1)] +
+                     tabulIn[ind2d(i+1,j )] + tabulIn[ind2d(i+1,j+1)];
 
-      if (tabulIn[ind2d(i,j)] && vizviv < 2)
-        tabulOut[ind2d(i,j)] = 0;
-      else if (tabulIn[ind2d(i,j)] && vizviv > 3)
-        tabulOut[ind2d(i,j)] = 0;
-      else if (!tabulIn[ind2d(i,j)] && vizviv == 3)
-        tabulOut[ind2d(i,j)] = 1;
-      else
-        tabulOut[ind2d(i,j)] = tabulIn[ind2d(i,j)];
+            if (tabulIn[ind2d(i,j)] && vizviv < 2)
+                tabulOut[ind2d(i,j)] = 0;
+            else if (tabulIn[ind2d(i,j)] && vizviv > 3)
+                tabulOut[ind2d(i,j)] = 0;
+            else if (!tabulIn[ind2d(i,j)] && vizviv == 3)
+                tabulOut[ind2d(i,j)] = 1;
+            else
+                tabulOut[ind2d(i,j)] = tabulIn[ind2d(i,j)];
+        }
     }
-  }
 }
 
-int main(void) {
-  omp_set_num_threads(16);
-  int pow, i, tam, *tabulIn, *tabulOut;
-  double t0, t1, t2, t3;
-
-  for (pow = POWMIN; pow <= POWMAX; pow++) {
-    tam = 1 << pow;
-
-    t0 = wall_time();
-    tabulIn  = (int ) malloc((tam+2)(tam+2)*sizeof(int));
-    tabulOut = (int ) malloc((tam+2)(tam+2)*sizeof(int));
-
-    t1 = wall_time();
-    for (i = 0; i < 2*(tam-3); i++) {
-      UmaVida(tabulIn, tabulOut, tam);
-      UmaVida(tabulOut, tabulIn, tam);
+void DumpTabul(int * tabul, int tam, int first, int last, char* msg) {
+    int i, ij;
+    printf("%s; Dump posições [%d:%d, %d:%d] de tabuleiro %d x %d\n", msg, first, last, first, last, tam, tam);
+    for (i = first; i <= last; i++) printf("="); printf("=\n");
+    for (i = ind2d(first,0); i <= ind2d(last,0); i += ind2d(1,0)) {
+        for (ij = i + first; ij <= i + last; ij++)
+            printf("%c", tabul[ij] ? 'X' : '.');
+        printf("\n");
     }
-    t2 = wall_time();
+    for (i = first; i <= last; i++) printf("="); printf("=\n");
+} /* fim-DumpTabul */
 
-    t3 = wall_time();
+void InitTabul(int* tabulIn, int* tabulOut, int tam) {
+    int ij;
+    for (ij = 0; ij < (tam+2)*(tam+2); ij++) {
+        tabulIn[ij] = 0;
+        tabulOut[ij] = 0;
+    }
+    tabulIn[ind2d(1,2)] = 1; tabulIn[ind2d(2,3)] = 1;
+    tabulIn[ind2d(3,1)] = 1; tabulIn[ind2d(3,2)] = 1;
+    tabulIn[ind2d(3,3)] = 1;
+} /* fim-InitTabul */
 
-    printf("tam=%d; tempos: init=%7.7f, comp=%7.7f, fim=%7.7f, tot=%7.7f \n",
-           tam, t1-t0, t2-t1, t3-t2, t3-t0);
+int Correto(int* tabul, int tam) {
+    int ij, cnt;
+    cnt = 0;
+    for (ij = 0; ij < (tam+2)*(tam+2); ij++)
+        cnt = cnt + tabul[ij];
+    return (cnt == 5 && tabul[ind2d(tam-2,tam-1)] &&
+            tabul[ind2d(tam-1,tam  )] && tabul[ind2d(tam  ,tam-2)] &&
+            tabul[ind2d(tam  ,tam-1)] && tabul[ind2d(tam  ,tam  )]);
+} /* fim-Correto */
 
-    // Enviar dados para Elasticsearch
-    send_to_elasticsearch(tam, t1-t0, t2-t1, t3-t0);
+int main(int argc, char** argv) {
+    omp_set_num_threads(16);
+    int pow, i, tam, *tabulIn, *tabulOut;
+    char msg[9];
+    double t0, t1, t2, t3;
+    int powmin = atoi(argv[1]);
+    int powmax = atoi(argv[2]);
 
-    free(tabulIn);
-    free(tabulOut);
-  }
+    // para todos os tamanhos do tabuleiro
+    for (pow = powmin; pow <= powmax; pow++) {
+        tam = 1 << pow;
+        // aloca e inicializa tabuleiros
+        t0 = wall_time();
+        tabulIn  = (int *) malloc ((tam+2)*(tam+2)*sizeof(int));
+        tabulOut = (int *) malloc ((tam+2)*(tam+2)*sizeof(int));
+        InitTabul(tabulIn, tabulOut, tam);
+        t1 = wall_time();
+        for (i = 0; i < 10; i++) {
+            UmaVida(tabulIn, tabulOut, tam);
+            UmaVida(tabulOut, tabulIn, tam);
+        }
+        t2 = wall_time();
 
-  return 0;
-}
+        if (Correto(tabulIn, tam))
+            printf("**OpenMP - RESULTADO CORRETO**\n");
+        else
+            printf("**OpenMP - RESULTADO ERRADO**\n");
+        t3 = wall_time();
+        printf("tam=%d; tempos: init=%7.7f, comp=%7.7f, fim=%7.7f, tot=%7.7f \n",
+               tam, t1-t0, t2-t1, t3-t2, t3-t0);
+
+        // Enviar dados para o Elasticsearch
+        char postdata[1024];
+        snprintf(postdata, sizeof(postdata), "{\"index\": \"jogo_da_vida\", \"data\": [");
+        for (i = 0; i < (tam+2)*(tam+2); i++) {
+            snprintf(postdata + strlen(postdata), sizeof(postdata) - strlen(postdata),
+                     "{\"position\": %d, \"value\": %d},", i, tabulIn[i]);
+        }
+        postdata[strlen(postdata) - 1] = ']'; // Substituir a última vírgula por colchete
+        strcat(postdata, "}");
+
+        // Adicione a função de envio de dados
+        SendDataToElasticsearch(postdata);
+
+        free(tabulIn);
+        free(tabulOut);
+    }
+    return 0;
+} /* fim-main */
